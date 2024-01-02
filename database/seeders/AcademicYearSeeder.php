e0ed64b5c84fe0dd37d864fcb9e7e5463000a768
etienne <etiennewayne@gmail.com>
2024-01-02 08:05:12 +0800

    update seeder

--- database/seeders/AcademicYearSeeder.php
+++ database/seeders/AcademicYearSeeder.php
@@ -17,12 +17,12 @@ class AcademicYearSeeder extends Seeder
 
         $data = [
             [
-                'academic_year_code' => '2023-2024',
+                'academic_year_code' => '23-24',
                 'academic_year_desc' => 'ACADEMIC YEAR 2023-2024',
                 'is_active' => 1
             ],
             [
-                'academic_year_code' => '2022-2023',
+                'academic_year_code' => '22-23',
                 'academic_year_desc' => 'ACADEMIC YEAR 2022-2023',
                 'is_active' => 0
             ]
